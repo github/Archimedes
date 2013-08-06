418fbb8317f0e42624e6b5c3e305498db443447c
Indragie Karunaratne <i@indragie.com>
2013-08-06 12:37:28 -0600

    Style

--- Archimedes/MEDEdgeInsets.m
+++ Archimedes/MEDEdgeInsets.m
@@ -14,16 +14,14 @@ MEDEdgeInsets MEDEdgeInsetsMake(CGFloat top, CGFloat left, CGFloat bottom, CGFlo
 	return (MEDEdgeInsets){top, left, bottom, right};
 }
 
-BOOL MEDEdgeInsetsEqualToEdgeInsets(MEDEdgeInsets insets1, MEDEdgeInsets insets2)
-{
+BOOL MEDEdgeInsetsEqualToEdgeInsets(MEDEdgeInsets insets1, MEDEdgeInsets insets2) {
 	return (fabs(insets1.top - insets2.top) < 0.1) &&
 		   (fabs(insets1.left - insets2.left) < 0.1) &&
 		   (fabs(insets1.bottom - insets2.bottom) < 0.1) &&
 		   (fabs(insets1.right - insets2.right) < 0.1);
 }
 
-CGRect MEDEdgeInsetsInsetRect(CGRect rect, MEDEdgeInsets insets)
-{
+CGRect MEDEdgeInsetsInsetRect(CGRect rect, MEDEdgeInsets insets) {
 	rect.origin.x += insets.left;
 #ifdef __IPHONE_OS_VERSION_MIN_REQUIRED
 	rect.origin.y += insets.top;
@@ -36,13 +34,11 @@ CGRect MEDEdgeInsetsInsetRect(CGRect rect, MEDEdgeInsets insets)
 	
 }
 
-NSString * NSStringFromMEDEdgeInsets(MEDEdgeInsets insets)
-{
+NSString * NSStringFromMEDEdgeInsets(MEDEdgeInsets insets) {
 	return [NSString stringWithFormat:@"{%g, %g, %g, %g}", insets.top, insets.left, insets.bottom, insets.right];
 }
 
-MEDEdgeInsets MEDEdgeInsetsFromString(NSString *string)
-{
+MEDEdgeInsets MEDEdgeInsetsFromString(NSString *string) {
 	MEDEdgeInsets insets = MEDEdgeInsetsZero;
 	if (string) {
 		sscanf(string.UTF8String, "{%lg, %lg, %lg, %lg}", &insets.top, &insets.left, &insets.bottom, &insets.right);
