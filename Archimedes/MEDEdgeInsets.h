48b9ced3eea46908eb258467fddae82e7f680064
Indragie Karunaratne <i@indragie.com>
2013-08-06 12:36:57 -0600

    Add MEDEdgeInsets typedef and geometry functions

---
+++ Archimedes/MEDEdgeInsets.h
@@ -0,0 +1,53 @@
+//
+//  MEDEdgeInsets.h
+//  Archimedes
+//
+//  Created by Indragie Karunaratne on 8/6/2013.
+//  Copyright (c) 2013 GitHub. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#ifdef __IPHONE_OS_VERSION_MIN_REQUIRED
+#import <UIKit/UIKit.h>
+#elif TARGET_OS_MAC
+#import <AppKit/AppKit.h>
+#endif
+
+#ifdef __IPHONE_OS_VERSION_MIN_REQUIRED
+typedef UIEdgeInsets MEDEdgeInsets;
+#elif TARGET_OS_MAC
+typedef NSEdgeInsets MEDEdgeInsets;
+#endif
+
+// `MEDEdgeInsets` structure with all members set to 0.
+extern MEDEdgeInsets const MEDEdgeInsetsZero;
+
+// Returns an MEDEgeInsets struct with the given edge insets.
+MEDEdgeInsets MEDEdgeInsetsMake(CGFloat top, CGFloat left, CGFloat bottom, CGFloat right);
+
+// Returns whether the two given `MEDEdgeInsets` are equal.
+BOOL MEDEdgeInsetsEqualToEdgeInsets(MEDEdgeInsets insets1, MEDEdgeInsets insets2);
+
+// Returns a rectangle adjusted by incrementing the origin and decrementing the size
+// of the given rect by applying the given insets.
+//
+// The behaviour of this function differs between Mac and iOS in that the Y origin of
+// the rect is incremented using the `top` member on iOS and the `bottom` member on Mac
+// due to the default flippedness of drawing contexts on each platform.
+CGRect MEDEdgeInsetsInsetRect(CGRect rect, MEDEdgeInsets insets);
+
+// Returns a string formatted to contain data from an `MEDEdgeInsets` structure.
+//
+// This string can be passed into `MEDEdgeInsetsFromString()` to recreate the original
+// `MEDEdgeInsets` structure.
+NSString * NSStringFromMEDEdgeInsets(MEDEdgeInsets insets);
+
+// Returns an `MEDEdgeInsets` structure corresponding to data in the given string
+// or `MEDEdgeInsetsZero` if the string is not formatted appropriately.
+//
+// The string format is “{top, left, bottom, right}”, where each member of the
+// `MEDEdgeInsets` structure is separated with a comma. This function should generally
+// only be used to convert strings that were previously created using the
+// `NSStringFromMEDEdgeInsets()` function.
+MEDEdgeInsets MEDEdgeInsetsFromString(NSString *string);
---
+++ Archimedes/MEDEdgeInsets.m
@@ -0,0 +1,51 @@
+//
+//  MEDEdgeInsets.m
+//  Archimedes
+//
+//  Created by Indragie Karunaratne on 8/6/2013.
+//  Copyright (c) 2013 GitHub. All rights reserved.
+//
+
+#import "MEDEdgeInsets.h"
+
+MEDEdgeInsets const MEDEdgeInsetsZero = {0, 0, 0, 0};
+
+MEDEdgeInsets MEDEdgeInsetsMake(CGFloat top, CGFloat left, CGFloat bottom, CGFloat right) {
+	return (MEDEdgeInsets){top, left, bottom, right};
+}
+
+BOOL MEDEdgeInsetsEqualToEdgeInsets(MEDEdgeInsets insets1, MEDEdgeInsets insets2)
+{
+	return (fabs(insets1.top - insets2.top) < 0.1) &&
+		   (fabs(insets1.left - insets2.left) < 0.1) &&
+		   (fabs(insets1.bottom - insets2.bottom) < 0.1) &&
+		   (fabs(insets1.right - insets2.right) < 0.1);
+}
+
+CGRect MEDEdgeInsetsInsetRect(CGRect rect, MEDEdgeInsets insets)
+{
+	rect.origin.x += insets.left;
+#ifdef __IPHONE_OS_VERSION_MIN_REQUIRED
+	rect.origin.y += insets.top;
+#elif TARGET_OS_MAC
+	rect.origin.y += insets.bottom;
+#endif
+	rect.size.height -= (insets.top + insets.bottom);
+	rect.size.width -= (insets.left + insets.right);
+	return rect;
+	
+}
+
+NSString * NSStringFromMEDEdgeInsets(MEDEdgeInsets insets)
+{
+	return [NSString stringWithFormat:@"{%g, %g, %g, %g}", insets.top, insets.left, insets.bottom, insets.right];
+}
+
+MEDEdgeInsets MEDEdgeInsetsFromString(NSString *string)
+{
+	MEDEdgeInsets insets = MEDEdgeInsetsZero;
+	if (string) {
+		sscanf(string.UTF8String, "{%lg, %lg, %lg, %lg}", &insets.top, &insets.left, &insets.bottom, &insets.right);
+	}
+	return insets;
+}
