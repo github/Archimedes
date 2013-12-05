c0624100cee7abfc9707d13a22b716ab4292a418
joshaber <joshaber@gmail.com>
2013-12-05 11:25:10 -0500

    In. Den. Ta. Tion.

--- Archimedes/CGGeometry+MEDConvenienceAdditions.m
+++ Archimedes/CGGeometry+MEDConvenienceAdditions.m
@@ -162,27 +162,27 @@ CGSize MEDSizeScale(CGSize size, CGFloat scale) {
 }
 
 CGSize MEDSizeScaleAspectFit(CGSize size, CGSize maxSize) {
-    CGFloat originalAspectRatio = size.width / size.height;
-    CGFloat maxAspectRatio = maxSize.width / maxSize.height;
+	CGFloat originalAspectRatio = size.width / size.height;
+	CGFloat maxAspectRatio = maxSize.width / maxSize.height;
 	CGSize newSize = maxSize;
-    if (originalAspectRatio > maxAspectRatio) {
-        newSize.height = maxSize.width / originalAspectRatio;
-    } else {
-        newSize.width = maxSize.height * originalAspectRatio;
-    }
+	if (originalAspectRatio > maxAspectRatio) {
+		newSize.height = maxSize.width / originalAspectRatio;
+	} else {
+		newSize.width = maxSize.height * originalAspectRatio;
+	}
 
-    return newSize;
+	return newSize;
 }
 
 CGSize MEDSizeScaleAspectFill(CGSize size, CGSize minSize) {
-    CGFloat scaleWidth = minSize.width / size.width;
-    CGFloat scaleHeight = minSize.height / size.height;
+	CGFloat scaleWidth = minSize.width / size.width;
+	CGFloat scaleHeight = minSize.height / size.height;
 
-    CGFloat scale = MAX(scaleWidth, scaleHeight);
-    CGFloat newWidth = size.width * scale;
-    CGFloat newHeight = size.height * scale;
+	CGFloat scale = fmax(scaleWidth, scaleHeight);
+	CGFloat newWidth = size.width * scale;
+	CGFloat newHeight = size.height * scale;
 
-    return CGSizeMake(newWidth, newHeight);
+	return CGSizeMake(newWidth, newHeight);
 }
 
 CGPoint MEDPointFloor(CGPoint point) {
